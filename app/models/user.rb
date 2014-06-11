2719a073a3a6adae1a242264dd2c677bc7650d33
shivamd <daryananis@gmail.com>
2014-06-10 19:48:06 -0700

    Subscriptions constant in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,15 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+    :recoverable, :rememberable, :trackable, :validatable
 
   has_many :tasks
   validates :name, :email, presence: true
   validates :email, uniqueness: true
+
+  SUBSCRIPTIONS = [
+    OpenStruct.new(type: "free", price: "", display_name: "Free"),
+    OpenStruct.new(type: "silver", price: "$9.99", display_name: "Silver $9.99/mo"),
+    OpenStruct.new(type: "gold", price: "$19.99", display_name: "Gold $19.99/mo"),
+  ]
 end
